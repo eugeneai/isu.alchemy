3566537cde98918ab9806c567ce28b8bc1f07f22
Evgeny Cherkashin <eugene@irnok.net>
2017-02-22 23:41:06 +0800

    Initial import

---
+++ tests/test_isu.alchemy.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class ../isu.alchemyTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        self.assertEqual(1 + 1, 2)
+
+    def tearDown(self):
+        pass
